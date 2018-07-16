1ada1ed3ed2c3282e6c89cdf16f198180fd80d5c
lewis prole <prolel@cardiff.ac.uk>
2018-07-16 11:38:17 +0100

    run_code should work now

--- run_code.py
+++ run_code.py
@@ -7,9 +7,11 @@ Created on Mon Jul 16 10:00:29 2018
 """
 import numpy as np
 import pandas as pd
+import csv
 import CODE #Tilly's file extraction code 
 
 '''
+Lewis' notes
 object type can be 'single' or 'multiple'
 -if single, set file_directory to 0.
 -if multiple, set RA,Dec,mag to 0
@@ -19,6 +21,7 @@ object type can be 'single' or 'multiple'
 
 #simple function allowing code to be run for single target
 #^ or csv file containing multiple targets 
+file_directory='fake_targets.csv'
 
 def run(object_type,file_directory,RA,Dec,mag):
     
@@ -28,9 +31,9 @@ def run(object_type,file_directory,RA,Dec,mag):
     
     if object_type=='multiple':
         data=pd.read_csv(file_directory)
-        for i in range (len(data[:,0])):
-            RA=data[i,0]
-            Dec=data[i,1]
-            mag=data[i,2]
+        for i in range (len(data)):
+            RA=data['RA'][i]
+            Dec=data['Dec'][i]
+            mag=data['mag'][i]
             CODE(RA,Dec,mag)
         pass
