a335c3c545057ed4231470e1c5912afb98e69814
tillyevans <40791446+tillyevans@users.noreply.github.com>
2018-08-21 13:57:34 +0100

    trying ElementTree to open xml

--- AAVSO_attempt2.py
+++ AAVSO_attempt2.py
@@ -5,19 +5,27 @@ Created on Thu Aug 16 14:25:38 2018
 @author: Tilly
 """
 import requests
-
-
-
-    
+import requests
+from xml.etree import ElementTree
 fromRA = 123
 fromDec = 123
 toRA = 150
 toDec = 140
-payload = {'fromra':fromRA,'tora':toRA, 'fromdec':fromDec,'todec':toDec}
-web = requests.request('GET','https://www.aavso.org/vsx/index.php?view=api.list', params = payload)
+
+payload = {'fromra':fromRA,
+           'tora':toRA, 
+           'fromdec':fromDec,
+           'todec':toDec}
+
+response = requests.get('https://www.aavso.org/vsx/index.php?view=api.list', params = payload)
 
 
-print(web)
+tree = ElementTree.fromstring(response.content)
+    
+
+
+
+
 
 
 
