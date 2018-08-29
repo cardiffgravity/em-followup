3965b530170737914ad91ec304975c7e9e75847b
lewis prole <prolel@cardiff.ac.uk>
2018-08-29 14:16:14 +0100

    added to the README

--- README.md
+++ README.md
@@ -14,9 +14,24 @@ Requests 2.14.2
 **Example of use:**
 
 ### download_data_LCO
+- module to download all LCO images taken with your proposal ID in a time frame
+- create txt file called userdata.txt
+- write:
+- LCOGT archive username, password, datafolder, and the name of the proposals
+- datafolder is the name of where you want to save files 
+- names of the proposals separated by commas 
 
+- e.g.
+- username = 
+- password = 
+- datafolder = 
+- proposals = 
 
 **Example of use:**
+
+```
+download(path_general,sdate,edate,proposalID,datafolder)
+```
 
 ### AAVSO_get_variables
 - The Zooniverse project asks users to circle the variations observed in images taken 5-7 days apart.
@@ -51,9 +66,59 @@ that field.
 ```
 
 ### imcreate
+- this module processes images to make them compatible and produces difference images, saving them as .png files.
+- create a general folder that contains the following empty folders:
+    
+- 'images'
+- 'LCO_images'
+- 'fits_images'
+- 'subtraction'
+- 'relative'
+- 'Zooniverse_upload'
+- 'rejects'
+- 'finished_images'
+
+- In 'images', create a folder per target to store target images
+- 2 comparison images of single target per target folder
+- images must have same name keyword as folder
+- e.g. target folder called 'keyword', images within called 'keyword_1', 'keyword_2'
+- name target folders using actual target name 
 
 
 **Example of use:**
-
+```
+path_general='/Users/lewisprole/Documents/University/year3/summer_project'
+remove_space(path_general,'images')
+remove_space(path_general,'LCO_images/raw')
+combine_fold(path_general)
+reject_dir(path_general)
+fz_remove(path_general)
+image_rename(path_general)
+CPR(path_general)
+bright_diff(path_general)
+sub(path_general)
+out_save(path_general,'yes','yes')
+```
 ### run_code
 
+- script to run the various python scripts to request, download and process images
+- all you need to do is enter your details and run the script 
+
+```
+#requests details
+username= "username"
+password = "password"
+RA = 202.4708
+Dec = 47.1953
+PROPOSAL_ID = "proposalID"
+magnitude = 9
+expt=30
+period =10000 #how often to check the if the images have downloaded
+path_general='path/to/general_folder'
+
+#download details
+sdate='2018-06-19'
+edate='2018-08-19'
+proposalID="LCOEPO2018A-004"
+datafolder="path/to/LCO_images"
+```
--- run_code.py
+++ run_code.py
@@ -20,32 +20,50 @@ import threading
 
 1) directories
 
-create general folder containing the followuing:
-an 'images' folder for pre-downloaded images.
-an 'LCO_images' folder where new LCO images are downloaded.
-create 'Zooniverse_upload' folder where processed images are saved.
-create empty folders called 'fits_images','subtraction','rejects','finished_images'
+create a general folder that contains the following empty folders:
+    
+'images'
+'LCO_images'
+'fits_images'
+'subtraction'
+'relative'
+'Zooniverse_upload'
+'rejects'
+'finished_images'
 
+For pre-downloaded images:
+in 'images', create a folder per target to store target images
+2 comparison images of single target per target folder
+images must have same name keyword as folder
+e.g. target folder called 'keyword', images within called 'keyword_1.fits', 'keyword_2.fits'
+name target folders using actual target name 
 
 2) making requests
 
-can request 'single' or 'multiple' targets
--if single, set file_directory to 0.
--if multiple, set RA,Dec,mag to 0
--multiple objects in csv file with columns RA,Dec,mag
+enter username, password, RA, DEC, proposal ID, object magnitude and exposure time 
+as exampled below.
 
 3) downloading data
 
--within 'images' folder, create a new folder per target
--2 images of the target per target file
--naming e.g. if target folder named 'object', images in folder must be named 'object_1' and 'object_2'
--call target folders whatever you like, but no duplicates
--define path_general directory leading to general folder containing 'images'/'LCO_images'
+-define variable path_general as directory leading to general folder containing 'images','LCO_images', etc.
 -also define directory 'datafolder' leading to LCO_images folder
 -enter LCO proposal ID and start/end date of download period
+-dates in yyyy-mm-dd format
+-set the period of how often the number of downloaded images is checked.
+-create txt file called userdata.txt in general folder
+write:
+LCOGT archive username, password, datafolder, and the name of the proposals
+datafolder (I think) is the name of where you want to save files 
+names of the proposals separated by commas 
+
+e.g.
+username = 
+password = 
+datafolder = 
+proposals = 
 
 Once the LCO request has been made, the download code will repeat periodically untill there are >30 targets 
-in the images folder, at which poiint the processing code runs. Set the 'period' variable in seconds.
+in the images & LCO_images folders combined, at which poiint the processing code runs. Set the 'period' variable in seconds.
 Once the processing code runs and saves the finished images to Zooniverse upload, it removes the originals 
 from the images folder.
 
@@ -55,13 +73,15 @@ from the images folder.
 -------------------------------------------------------------
 '''
 
-username= "lewisprole"
-password = "Walkingdead66"
+username= "username"
+password = "password"
 RA = 202.4708
 Dec = 47.1953
-PROPOSAL_ID = "LCOEPO2018A-004"
+PROPOSAL_ID = "proposalID"
 magnitude = 9
 expt=30
+period =10000
+path_general='path/to/general_folder'
 
 
 ''' REQUEST LCO IMAGES TO BE TAKEN
@@ -69,37 +89,33 @@ expt=30
 '''
 
 #run request
-#userrequest = Request(RA, Dec,magnitude,PROPOSAL_ID,username,password)
-#response = requests.post(
-#    'https://observe.lco.global/api/userrequests/',
-#    headers = userrequest.retrieve_token(username, password),
-#    json = userrequest.final_for_submission(RA, Dec, magnitude, PROPOSAL_ID)  
-#)
-#try:
-#    response.raise_for_status()
-#except requests.exceptions.HTTPError as exc:
-#    print('Request failed: {}'.format(response.content))
-#    raise exc
-#    
-#userrequest_dict = response.json()  # The API will return the newly submitted userrequest as json
-#
-## Print out the url on the portal where we can view the submitted request
-#print('View this observing request: https://observe.lco.global/userrequests/{}/'.format(userrequest_dict['id']))
+userrequest = Request(RA, Dec,magnitude,PROPOSAL_ID,username,password)
+response = requests.post(
+    'https://observe.lco.global/api/userrequests/',
+    headers = userrequest.retrieve_token(username, password),
+    json = userrequest.final_for_submission(RA, Dec, magnitude, PROPOSAL_ID)  
+)
+try:
+    response.raise_for_status()
+except requests.exceptions.HTTPError as exc:
+    print('Request failed: {}'.format(response.content))
+    raise exc
+    
+userrequest_dict = response.json()  # The API will return the newly submitted userrequest as json
+
+# Print out the url on the portal where we can view the submitted request
+print('View this observing request: https://observe.lco.global/userrequests/{}/'.format(userrequest_dict['id']))
 
 
-''' SET UP DIRECTORY TO FOLDER CONTAINING 'IMAGES' FOLDER
+''' ENTER DOWNLOAD/PROCESSING DETAILS
 -------------------------------------------------------------
-'''
-
-#enter dOWnload/processing details
-
+e.g. 
 sdate='2018-06-19'
 edate='2018-08-19'
 proposalID="LCOEPO2018A-004"
 path_general="/Users/lewisprole/Documents/University/year3/summer_project"
-#path_general='/Libraries/Documents/summer_project/em_followup_master'
 datafolder="/Users/lewisprole/Documents/University/year3/summer_project/LCO_images"
-spectra=False
+'''
 
 
 ''' DOWNLOAD AND PROCESS IMAGES 
@@ -110,11 +126,12 @@ spectra=False
 #checks to see if there are 30 targets in image folder
 #runs image processing code if >30 images 
 def repeat(period,path_general):
+    spectra=False
     download_data_LCO.download(path_general,sdate,edate,proposalID,datafolder)
     imcreate.fold_check(path_general)
     threading.Timer(period, repeat).start()
 
-
+repeat(period,path_general)
     
 
 
